21334375f44c74ae6eb61777f5060423efad3e67
Aleksey Yesin <esinru@gmail.com>
2021-03-20 03:22:55 +0200

    Save 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import './styles.scss';
+import pixApi from './js/pixApi';
+import startTpl from './templates/start.hbs';
 
-// getImagesByQuery()
-// console.log(undefined > -1);
+const refs = {
+    galleryList: document.querySelector('#gallery-list'),
+};
+
+pixApi.getImagesByQuery({ name: 'ff' }).then(res => {
+    refs.galleryList.innerHTML = startTpl(res.hits);
+});
--- src/js/pixApi.js
+++ src/js/pixApi.js
@@ -1,3 +1,5 @@
+import queryParams from '../json/queryParams.json';
+
 export default {
     _ENDPOINT: 'https://pixabay.com/api/',
     _API_KEY: '20679339-fea13a2297aa7649e9595d106',
@@ -6,7 +8,7 @@ export default {
         return fetch(`${this._ENDPOINT}?key=${this._API_KEY}${paramsStr}`).then(
             resp => {
                 if (resp.ok) {
-                    return response.json();
+                    return resp.json();
                 }
 
                 throw new Error('request was rejected by server');
@@ -31,5 +33,24 @@ export default {
         if (typeof page === 'number' && page >= 1) {
             paramsStr += `&page=${page}`;
         }
+
+        if (typeof perPage === 'number' && perPage >= 3 && perPage <= 200) {
+            paramsStr += `&per_page=${perPage}`;
+        }
+
+        if (queryParams.CATEGORIES.includes(category)) {
+            paramsStr += `&category=${category}`;
+        }
+
+        if (queryParams.TYPES.includes(type)) {
+            paramsStr += `&image_type=${type}`;
+        }
+
+        if (queryParams.ORIENTATIONS.includes(orientation)) {
+            paramsStr += `&orientation=${orientation}`;
+        }
+
+        console.log(paramsStr);
+        return this._getImages(paramsStr);
     },
 };
