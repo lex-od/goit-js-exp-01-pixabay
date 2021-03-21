816f0eb19eaebb5448ee1700db6664c7a45d317a
Aleksey Yesin <esinru@gmail.com>
2021-03-21 14:14:20 +0200

    Dynamic card colors v0.5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Handlebars from 'handlebars';
+import * as Handlebars from 'handlebars/runtime';
 
 import './styles.scss';
 import pixApi from './js/pixApi';
@@ -8,11 +8,11 @@ const refs = {
     galleryList: document.querySelector('#gallery-list'),
 };
 
-Handlebars.registerHelper('zzz', function (likesCount) {
+Handlebars.registerHelper('dyn-likes', function (likesCount) {
     let classNum = Math.floor(likesCount / 20);
     classNum = classNum > 10 ? 10 : classNum;
 
-    console.log(classNum);
+    console.log(likesCount, classNum);
     return classNum;
 });
 
@@ -21,7 +21,8 @@ Init();
 function Init() {
     pixApi.setStartSearchParams({
         name: 'cat',
-        page: 10,
+        page: 1,
+        perPage: 100,
         type: 'photo',
         orientation: 'horizontal',
     });
